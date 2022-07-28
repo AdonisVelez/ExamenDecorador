b9b01f2ca288514984c90d3522dbd2bf7f0c9e39
AdonisVelez <106783832+AdonisVelez@users.noreply.github.com>
2022-07-28 16:12:23 -0500

    Update HeladoBase.cs

--- HeladoBase.cs
+++ HeladoBase.cs
@@ -2,14 +2,7 @@ using System;
 
 namespace Examen {
 public abstract class HeladoBase{
-    /*
-    protected List<Helado> Helados = new List<Helado>
-    {
-        new Helado {Sabor = "Chocolate", Costo = 1.50},
-        new Helado {Sabor = "Fresa", Costo = 1},
-        new Helado {Sabor = "Chicle", Costo = 1.25}
-    };
-    */
+    
     public abstract double CalculoTotalPrecio();
     }
 }
