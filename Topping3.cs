aa47fff02bec0382911eeb3dbae1f61f1c996fb2
AdonisVelez <106783832+AdonisVelez@users.noreply.github.com>
2022-07-28 16:16:01 -0500

    Update Topping3.cs

--- Topping3.cs
+++ Topping3.cs
@@ -9,8 +9,6 @@ public class Topping3 : Decorador
 
     public override double CalculoTotalPrecio()
     {
-        //Console.WriteLine("Se añade topping 3");
-        //Console.Write("Nuevo Precio del ");
         var Adiccion = base.CalculoTotalPrecio() + 0.35;
         return Adiccion;
     }
