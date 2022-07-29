e9fc974752ac664ace55f39f0b885bb5f81052f4
AdonisVelez <106783832+AdonisVelez@users.noreply.github.com>
2022-07-29 09:16:10 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,18 +7,22 @@ namespace Examen
         public static void Main(string[] arg)
         {
 
-            var Helado1 = new HeladoChocolate();
-            Console.WriteLine("$"+Helado1.CalculoTotalPrecio());
-
-            var Adereso1 = new Topping1(Helado1);
-            Console.WriteLine("$"+Adereso1.CalculoTotalPrecio());
-
-            var Adereso2 = new Topping2(Adereso1);
-            Console.WriteLine("$"+Adereso2.CalculoTotalPrecio());
-
+            var chocoate = new HeladoChocolate();
+            Console.WriteLine("$"+chocolate.CalculoTotalPrecio());
+            var adereso1 = new Topping1(chocolate);
+            Console.WriteLine("$"+adereso1.CalculoTotalPrecio());
+            var adereso2 = new Topping2(adereso1);
+            Console.WriteLine("$"+adereso2.CalculoTotalPrecio());
             Console.WriteLine();
 
-         
+            var fresa = new HeladoFresa();
+            Console.WriteLine("$"+frsa.CalculoTotalPrecio());
+            var adereso3 = new Topping1(chocolate);
+            Console.WriteLine("$"+adereso3.CalculoTotalPrecio());
+            var adereso4 = new Topping2(adereso3);
+            Console.WriteLine("$"+adereso2.CalculoTotalPrecio());
+            Console.writeLine();
+
         }
     }
 }
