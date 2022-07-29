d4a8e993cd05dafc7b5725ba8b9c93a5015c5984
AdonisVelez <106783832+AdonisVelez@users.noreply.github.com>
2022-07-29 09:08:10 -0500

    Update HeladoChocolate.cs

--- HeladoChocolate.cs
+++ HeladoChocolate.cs
@@ -6,8 +6,8 @@ public class HeladoChocolate : HeladoBase
     public override double CalculoTotalPrecio()
     {
         Console.Write("Total del Helado: ");
-        var H1 = 1.50;
-        return H1;
+        var valor = 1.50;
+        return valor;
     }
 }
 }
