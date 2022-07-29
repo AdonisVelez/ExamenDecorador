114e632e30048976262185567f0b65676fd72241
AdonisVelez <106783832+AdonisVelez@users.noreply.github.com>
2022-07-29 09:07:20 -0500

    Update HeladoChicle.cs

--- HeladoChicle.cs
+++ HeladoChicle.cs
@@ -6,8 +6,8 @@ public class HeladoChicle : HeladoBase
     public override double CalculoTotalPrecio()
     {
         Console.Write("Total del Helado: ");
-        var H2 = 1.25;
-        return H2;
+        var valor = 1.25;
+        return valor;
     }
 }
 }
